880b1c9a9ce8cd9ac78c8a5237207e62d6a13e7a
Zuzanna Dubanowska <2312052d@student.gla.ac.uk>
2020-02-11 10:23:17 -0800

    Working chatbot dummy appusing Dialogflow

---
+++ App.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Dialogflow_V2} from 'react-native-dialogflow';
+import {GiftedChat} from 'react-native-gifted-chat';
+import {dialogflowConfig} from './config';
+
+const BOT_USER = {
+  _id: 2,
+  name: 'Glooko Buddy',
+  avatar: 'https://media.glassdoor.com/sql/1320444/glooko-squarelogo-1467383473350.png',
+};
+
+class App extends Component {
+  state = {
+    messages: [
+      {
+        _id: 1,
+        text:
+          'Hi! I am your Glooko Buddy 🤖.\n\nI am here to answer your questions about diabetes. ',
+        createdAt: new Date(),
+        user: BOT_USER,
+      },
+    ],
+  };
+
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id,
+    );
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleResponse(result),
+      error => console.log(error),
+    );
+  }
+
+  handleResponse(result) {
+    console.log(result);
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    let payload = result.queryResult.webhookPayload;
+    this.showResponse(text, payload);
+  }
+
+  showResponse(text, payload) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER,
+    };
+
+    if (payload && payload.is_image) {
+      msg.text = text;
+      msg.image = payload.url;
+    }
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, [msg]),
+    }));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+      </View>
+    );
+  }
+}
+//
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ config.js
@@ -0,0 +1,12 @@
+export const dialogflowConfig ={
+  "type": "service_account",
+  "project_id": "glookobuddy-frddnv",
+  "private_key_id": "9552a8f6df20e240e7715d0f577f2c7867ef51d4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCxLK9IUBd2k/6+\n3Y1yEY4iDeTWipRzF/aeULErCAvo/bobTdE3gfFMTP7doPy6hOD45jGqzIjYbUik\nUXB+XgobvmEq9HJpEv66SoReI9Kk4jZRdBF+GLReGNLI1grOOepvOgytbABdf1ft\nipMn2NxlWOgm0l32jn9FCjqzxoN5SZHgDhYtGNENoySe5tbX6oX0rt1nL5OYhB1c\nKFLMi+pav//wI0YPVPva49J/NrZU5Kl6oiM3i4Ln7LbwvHZRHPm8l0Ne9v+PBiQF\nlIuo1IUp3b96aiHl/7LjwGJpjOpE5gS3T7VmwT9jWwnAGnVkU01/e3/QMzFTt3FA\nAYmurYedAgMBAAECggEAIqsHdlsjqDJ/yamxBHW+bdfxo/BUaWvyOBq6fBzUNcg2\nGyMgMeAVjvRO95+JbFbVbaafZ9gjW9QQf4SnAjcU+8ZGHaFc5MYQfXPY0FHqpRRV\nhB010gpz+qiZUC6fySgogRCnC+Ui8iiZlDPGJSkoQRWDQZtuwvCxoxkEEQriTYA9\nkXfOM+GdD1yKlWT85dcxw3kPvl7S7r806CGcg6ZTxN3rLtkCPX/a15qVC8COndPz\n/PIxDdIk3yD7MqM47P5uH1oqq8EiLIeCcqYzkzVocsoOIR3jB/sc7Wjkblca39GK\nnZxsn6MV4vJ1hpabI/+ZqepaOHmJJtoH0C11UUQXUQKBgQDxT1WfP8KGV8k9Sgc+\ncaAMnvefuWlhPmdMZvmwOyLy4vuwFQ7wIfYTe6QmB/IDD0jlEH5tyzLmHcxhdIMP\nfX/DLRD+qW4SD51f5k8KcOPwPZ92kUyJOQwvnqMY6CGLtEpYRpAAero5ncYdpNKK\njYV3zk0ujjRzVML1FHMgfFWQkQKBgQC79dcowN+NptJxjmzaNLYIRXHJFN3XfTrI\nVkREWPzmGJ/nS2HZniLHBlcXnYpQUVw6k87+8Qp9Eqy0+cch5nJx9bRAqw7n0zCr\nry+9m7cJDavUA5nkjrRXgXdNpKnN48zolzyUkNeqOdvrzRp1BUXjhCJaFbojDj98\n0dy+5KZMTQKBgQDtM2Ph70Xi0sYRBgTK39xIq7aTfilVGodl9c846Vh4UqC/PGP7\nrhZ6nk4DyEmfslWgPS9Z86TryJzPqzztbBq7m0AaZCe7fzfePcCybbfRvnFSlo8a\nUEZnRNPjtsLK1gVCctI4dms6GZnWJ9+BrI1m8Ba0TbrHJ6mgSWLFqxJc0QKBgQCH\nfMG4rbc3u33Mv1a1v/NLaQuJYBI9iBa8XHrfLao3Dp8NMeh53qTBBE/5ROVz2gH6\nc5ttaEEgQTrfRYGGbUAn524kg2m+MBzQVPq72agJzaIDtQjUmQ1kWn2uKTkBoeiN\n7J6NlQ7wtL5TbkhY/BER3KiKeUjwhg+7oOdXbyh0rQKBgAJVAtdHCmFW76hn29R5\nDa1khFtj8BlvBj15uCio1Mdebj187I8bAtTHXYGp7x55HUmTTkjZ48XumAkaHAC+\npdiJHofVLh3KJu533uX88Szdz25+/Tzl715X+qZ4RXpo8g8C1bsy9mXr3qmsjD4n\n1dheM5YdzMBlfii8BDMckkHf\n-----END PRIVATE KEY-----\n",
+  "client_email": "dialogflow-tycwad@glookobuddy-frddnv.iam.gserviceaccount.com",
+  "client_id": "103355878508754214602",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-tycwad%40glookobuddy-frddnv.iam.gserviceaccount.com"
+};
